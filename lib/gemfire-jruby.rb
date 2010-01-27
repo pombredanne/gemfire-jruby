19b4c1d56f92759d9d537b582b63809411894cef
amckean <alanmckean@me.com>
2010-01-27 11:42:20 -0800

    Added docs directory and RDoc-generated files

--- lib/gemfire-jruby.rb
+++ lib/gemfire-jruby.rb
@@ -10,6 +10,7 @@ include Java
 
 module ActiveSupport
   module Cache
+    # ActiveSupport::Cache::GemFire creates a Singleton object that provides access to a GemFire cache.
     class GemFire < Store
       class << self; attr_accessor :instance; end
       
@@ -17,8 +18,13 @@ module ActiveSupport
   
     	private_class_method :new
       
+    	# GemFire is a Singleton. new() is hidden, so use getInstance() to both create the GemFire instance and to launch GemFire.
+    	#   There is an optional Hash that you can use to override any GemFire properties'.
+    	#   For example, GemFire.getInstance('locators' => 'localhost[10355]', 'mcast-port' => '0')
+    	# Since it is a Singleton, successive calls to GemFire.getInstance() will return the single
+    	# instance that was instantiated by the first call.
     	def GemFire.getInstance(hashOfGemFireProperties)
-      	self.instance ||= new(hashOfGemFireProperties)
+      	self.instance ||= new(hashOfGemFireProperties={})
       end
 
       def initialize(hashOfGemFireProperties)
@@ -34,6 +40,7 @@ module ActiveSupport
           false
     	end
 
+      # Read a value from the GemFire cache. _key_ can be any JRuby object. Returns the value stored at _key_.
       def read(key)
         super
         @region.get(key)
@@ -42,6 +49,7 @@ module ActiveSupport
           false
       end
 
+      # Write a value to the GemFire cache. _key_ is used to read the value from the cache and can be any JRuby object. Returns the value that was stored at _key_.
       def write(key, value)
         super
         @region.put(key, value)
@@ -51,6 +59,7 @@ module ActiveSupport
         false
       end
 
+      # Delete the entry stored in the GemFire cache at _key_. _key_ can be any JRuby object. Returns the value that was deleted.
       def delete(key)
         super
         @region.destroy(key)
@@ -59,16 +68,18 @@ module ActiveSupport
         false
       end
 
+      # Fetch all of the keys currently in the GemFire cache. Returns a JRuby Array of JRuby objects.
       def keys
-        super
         @region.keys.to_a
       end
 
+      # Check if there is an entry accessible by _key_ in the GemFire cache. Returns a boolean.
       def exist?(key)
         super
         @region.containsKeyOnServer(key)
       end
 
+      # Delete all entries (key=>value pairs) from the GemFire cache. Returns a JRuby Hash.
       def clear
         super
         @region.clear
@@ -78,14 +89,17 @@ module ActiveSupport
         false
       end
 
+      # Not implemented by GemFire. Raises an exception when called.
       def increment(key)
         raise "Not supported by Gemfire"
       end
 
+      # Not implemented by GemFire. Raises an exception when called.
       def decrement(key)
         raise "Not supported by Gemfire"
       end
 
+      # Not implemented by GemFire. Raises an exception when called.
       def delete_matched(matcher)
         raise "Not supported by Gemfire"
       end
